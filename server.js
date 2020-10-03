e950511cba2b9fea6786880f5797febad48dc8be
Z-Reaper <71580072+Z-Reaper@users.noreply.github.com>
2020-10-03 16:20:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const Dlang = require('discordbot-script')
 const discordScript = require("discordbot-script")
 
 const bot = new discordScript({
-  token: "NzYxNTQ5Mjk4OTEwNzU2ODg1.X3cODQ.cRDO4UgGpd_cxJRkBNdfR21cAGU",
-  prefix: ["!"]
+  token: "TOKEN",
+  prefix: ["PREFIX"]
 })
